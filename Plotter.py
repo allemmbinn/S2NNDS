b8b8ef08f090b9be1312d6f10fd1ab3c879fab84
mahathi-anand <mahathi.anand@gmail.com>
2025-06-06 17:06:29 +0200

    reproducibility

--- Plotter.py
+++ Plotter.py
@@ -24,6 +24,36 @@ def initialDSPlot(model_f, demos, initial_set_center, dim_in, config, model_b=No
                 x[j] = x[j-1] + Fout * dt
             x = x.cpu().detach().numpy()
             ax.plot(x[:, 0], x[:, 1],color[i], label=f'Final Trajectory {i+1}')
+
+        # Plotting the Safe Region
+        if model_b is not None:
+            len_sample = [128, 128]
+            RANGE = config["plotting"].get("range", [[-1, 1], [-1, 1]])
+            x = np.linspace(RANGE[0][0], RANGE[0][1], len_sample[0])
+            y = np.linspace(RANGE[1][0], RANGE[1][1], len_sample[1])
+            X, Y = np.meshgrid(x, y)
+            # Convert X and Y to torch tensors
+            X_tensor = torch.tensor(X, dtype=torch.float32).to(device)
+            Y_tensor = torch.tensor(Y, dtype=torch.float32).to(device)
+            input_data = torch.stack((X_tensor, Y_tensor), dim=-1).reshape(-1, 2).to(device)
+            unflatten = torch.nn.Unflatten(0, len_sample)
+            # Streamplot
+            with torch.no_grad():
+                F_out = model_f(input_data)
+                vect_out = unflatten(F_out)
+                vect_out = vect_out.cpu().detach().numpy()
+                U = vect_out[:,:, 0]
+                V = vect_out[:,:,1]
+                B_out = model_b(input_data)
+                bout = unflatten(B_out).cpu().detach().numpy()
+            stream = ax.streamplot(X, Y, U, V, density=2, linewidth=1, color='#a5a1a1')
+            # Create proxy artist for streamplot
+            arrow_proxy = mpl.lines.Line2D([0], [0], linestyle='-', color='#a5a1a1', marker='>', markeredgewidth=2, markersize=5, label='Vector Field')
+            plt.contour(X, Y, bout[:,:,0], levels=[0], colors='#cdebc5')
+            plt.contourf(X, Y, bout[:,:,0], levels=[-np.inf, 0], colors='#cdebc5')
+            contour_fill_legend = mpl.patches.Patch(color='#cdebc5', label=' $ \{x \in X \mid \mathrm{B}(x) \leq 0\}$')        
+            # Plotting the Invariant Set
+    
         # Plotting the Unsafe Set
         unsafe_config = config["unsafe"]
         unsafe_shape = unsafe_config["shape"]
@@ -75,34 +105,6 @@ def initialDSPlot(model_f, demos, initial_set_center, dim_in, config, model_b=No
             x,y = np.meshgrid(x, y)
             mask = (eval(function) <= 0)
             plt.contourf(x, y, mask.astype(int), levels = [0.5, 1], colors = 'black', linewidths=2, label = "Unsafe Set", alpha = 0.5)
-        # Plotting the Safe Region
-        if model_b is not None:
-            len_sample = [128, 128]
-            RANGE = config["plotting"].get("range", [[-1, 1], [-1, 1]])
-            x = np.linspace(RANGE[0][0], RANGE[0][1], len_sample[0])
-            y = np.linspace(RANGE[1][0], RANGE[1][1], len_sample[1])
-            X, Y = np.meshgrid(x, y)
-            # Convert X and Y to torch tensors
-            X_tensor = torch.tensor(X, dtype=torch.float32).to(device)
-            Y_tensor = torch.tensor(Y, dtype=torch.float32).to(device)
-            input_data = torch.stack((X_tensor, Y_tensor), dim=-1).reshape(-1, 2).to(device)
-            unflatten = torch.nn.Unflatten(0, len_sample)
-            # Streamplot
-            with torch.no_grad():
-                F_out = model_f(input_data)
-                vect_out = unflatten(F_out)
-                vect_out = vect_out.cpu().detach().numpy()
-                U = vect_out[:,:, 0]
-                V = vect_out[:,:,1]
-                B_out = model_b(input_data)
-                bout = unflatten(B_out).cpu().detach().numpy()
-            stream = ax.streamplot(X, Y, U, V, density=2, linewidth=1, color='#a5a1a1')
-            # Create proxy artist for streamplot
-            arrow_proxy = mpl.lines.Line2D([0], [0], linestyle='-', color='#a5a1a1', marker='>', markeredgewidth=2, markersize=5, label='Vector Field')
-            plt.contour(X, Y, bout[:,:,0], levels=[0], colors='#cdebc5')
-            plt.contourf(X, Y, bout[:,:,0], levels=[-np.inf, 0], colors='#cdebc5')
-            contour_fill_legend = mpl.patches.Patch(color='#cdebc5', label=' $ \{x \in X \mid \mathrm{B}(x) \leq 0\}$')        
-            # Plotting the Invariant Set
         ax.set_xlabel('X Label')
         ax.set_ylabel('Y Label')
         plt.title('Trajectories of the Dynamical System')
